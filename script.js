b5c422ac2898675a1056f6b7efa7713148b82e30
A-Goffard <aintzagoffard@gmail.com>
2024-01-24 13:30:24 +0100

    me esta emparanollando ya

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-//funcion para iniciar canva
-
-let canvas = document.querySelector("canvas");
-
-let ctx = canvas.getContext("2d"); 
-let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
-let str = ["B", " ", "I", " ", "L"," ", "B"," ", "O"," ", "S"," ", "T"," ", "A"," ", "C", "", "K"];
-let matrix = str.sort();
-let font = 5;
-let col = width / font;
-let arr = [];
-
-for(let i = 0; i < col; i++) {
-    arr[i] = 1;
-}
-
-const draw = () => {
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
-    ctx.fillRect(0, 0, width, height);
-    ctx.fillStyle = "#E129FF"; 
-    ctx.font = `$(font)px system-iu`;
-
-    for(let i = 0; i < arr.length; i++) {
-        let txt = matrix[Math.floor(Math.random()  * matrix.length)];
-        ctx.fillText(txt, i * font, arr[i] * font);
-
-        if(arr[i] * font > height && Math.random() > 0.975) {
-            arr[i] = 0;
-        }
-
-        arr[i]++;
-    }
-}
-
-setInterval(draw, 15);
-
-window.addEventListener("resize", () => location.reload());
-
-
-
-
 "use strict";
 
 // Variable para rastrear si el audio ya se reprodujo
@@ -97,3 +55,41 @@ function playAudio() {
     }
 }
 
+//funcion para iniciar canva
+
+let canvas = document.querySelector("canvas");
+
+let ctx = canvas.getContext("2d"); 
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+let str = ["B", " ", "I", " ", "L"," ", "B"," ", "O"," ", "S"," ", "T"," ", "A"," ", "C", "", "K"];
+let matrix = str.sort();
+let font = 5;
+let col = width / font;
+let arr = [];
+
+for(let i = 0; i < col; i++) {
+    arr[i] = 1;
+}
+
+const draw = () => {
+    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#E129FF"; 
+    ctx.font = `$(font)px system-iu`;
+
+    for(let i = 0; i < arr.length; i++) {
+        let txt = matrix[Math.floor(Math.random()  * matrix.length)];
+        ctx.fillText(txt, i * font, arr[i] * font);
+
+        if(arr[i] * font > height && Math.random() > 0.975) {
+            arr[i] = 0;
+        }
+
+        arr[i]++;
+    }
+}
+
+setInterval(draw, 15);
+
+window.addEventListener("resize", () => location.reload());
